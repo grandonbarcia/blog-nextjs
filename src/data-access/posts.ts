41c17a67c8b58168dc2848f8683f743a6d1fe1f7
Brandon Garcia <grandonleebarcia@gmail.com>
2024-03-20 13:48:59 -0700

    added getPostbyId function

--- src/data-access/posts.ts
+++ src/data-access/posts.ts
@@ -3,3 +3,11 @@ import prisma from '../../prisma/client';
 export async function getAllPosts() {
   return await prisma.post.findMany();
 }
+
+export async function getPostById(id: number) {
+  return await prisma.post.findUnique({
+    where: {
+      id: id,
+    },
+  });
+}
